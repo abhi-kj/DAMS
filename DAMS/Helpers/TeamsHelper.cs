47450f9a83b5bddde6d3933a6eacc25acbaa3666
Abhishek Kr Jha <abhishek.kumar.jha@intel.com>
2025-03-04 22:46:52 +0530

    Added format for private Chat

--- DAMS/Helpers/TeamsHelper.cs
+++ DAMS/Helpers/TeamsHelper.cs
@@ -1,5 +1,6 @@
 ﻿
 using DAMS.DTO;
+using System.Text;
 
 namespace DAMS.Helpers
 {
@@ -69,8 +70,9 @@ namespace DAMS.Helpers
         }
 
 
-        public async Task SendDailyReportAsync(ReportData reportData)
+        public async Task SendDailyReportAsyncChannel(ReportData reportData)
         {
+
             using var _httpClient = new HttpClient();
             var currentDate = DateTime.UtcNow.AddHours(5.5).ToString("yyyy-MM-dd HH:mm:ss"); // IST time
             var messageCard = new
@@ -94,7 +96,8 @@ namespace DAMS.Helpers
                     },
                     new
                     {
-                        activityTitle = "<hr style='border:2px solid blue; width:100%' />",
+                        activityTitle = "<span style='color:blue'>________________________</span>",
+
                         facts = new[]
                         {
                             new { name = "", value = "" }
@@ -114,6 +117,93 @@ namespace DAMS.Helpers
 
             await _httpClient.PostAsJsonAsync(_webhookUrl, messageCard);
         }
+
+        public async Task SendDailyReportAsync(ReportData reportData)
+        {
+            using var _httpClient = new HttpClient();
+            var currentDate = DateTime.UtcNow.AddHours(5.5).ToString("yyyy-MM-dd HH:mm:ss"); // IST time
+            //var x = "{\r\n  \"type\": \"message\",\r\n  \"attachments\": [\r\n    {\r\n      \"contentType\": \"application/vnd.microsoft.card.adaptive\",\r\n      \"content\": {\r\n        \"$schema\": \"http://adaptivecards.io/schemas/adaptive-card.json\",\r\n        \"type\": \"AdaptiveCard\",\r\n        \"version\": \"1.2\",\r\n        \"body\": [\r\n          {\r\n            \"type\": \"TextBlock\",\r\n            \"text\": \"Hello, this is a message sent via Power Automate!\",\r\n            \"wrap\": true\r\n          }\r\n        ],\r\n        \"actions\": [\r\n          {\r\n            \"type\": \"Action.OpenUrl\",\r\n            \"title\": \"Learn More\",\r\n            \"url\": \"https://adaptivecards.io\"\r\n          }\r\n        ]\r\n      }\r\n    }\r\n  ]\r\n}";
+            var messageCard = new
+            {
+                type = "message",
+                attachments = new[]
+                {
+                    new
+                    {
+                        contentType = "application/vnd.microsoft.card.adaptive",
+                        content = new
+                        {
+                            xschema = "http://adaptivecards.io/schemas/adaptive-card.json",
+                            type = "AdaptiveCard",
+                            version = "1.2",
+                            body = new[]
+                            {
+                                new
+                                {
+                                    type = "TextBlock",
+                                    text = $"Update on {currentDate}",
+                                    wrap = true
+                                },
+                                new
+                                {
+                                    type = "TextBlock",
+                                    text = "Daily Mail Report of ETMP Production",
+                                    wrap = true
+                                },
+                                new
+                                {
+                                    type = "TextBlock",
+                                    text = $"Successful Mails: <span style='color:green'><b>{reportData.MailReportData.MailSuccessCount}</b></span> messages were sent successfully.",
+                                    wrap = true
+                                },
+                                new
+                                {
+                                    type = "TextBlock",
+                                    text = reportData.MailReportData.MailFailCount > 0 ? $"Failed Mails: <span style='color:red'><b>{reportData.MailReportData.MailFailCount}</b></span> messages failed to send." : $"Failed Mails: {reportData.MailReportData.MailFailCount} messages failed to send.",
+                                    wrap = true
+                                },
+                                new
+                                {
+                                    type = "TextBlock",
+                                    text = $"Skipped Mails: <b>{reportData.MailReportData.SkipCount}</b> messages were skipped.",
+                                    wrap = true
+                                },
+                                new
+                                {
+                                    type = "TextBlock",
+                                    text = "________________________",
+                                    wrap = true
+                                },
+                                new
+                                {
+                                    type = "TextBlock",
+                                    text = "Daily Sync Report of ETMP Production",
+                                    wrap = true
+                                },
+                                new
+                                {
+                                    type = "TextBlock",
+                                    text = $"Successful Job Syncs: <span style='color:green'><b>{reportData.SyncReportData.SyncSuccessCount}</b></span> syncs were successful.",
+                                    wrap = true
+                                },
+                                new
+                                {
+                                    type = "TextBlock",
+                                    text = reportData.SyncReportData.SyncFailCount > 0 ? $"Failed Job Syncs: <span style='color:red'><b>{reportData.SyncReportData.SyncFailCount}</b></span> syncs failed." : $"Failed Job Syncs: {reportData.SyncReportData.SyncFailCount} syncs failed.",
+                                    wrap = true
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            var messageCardJson = JsonConvert.SerializeObject(messageCard);
+            messageCardJson = messageCardJson.Replace("xschema", "$schema");
+            await _httpClient.PostAsync(_webhookUrl, new StringContent(messageCardJson, Encoding.UTF8, "application/json"));
+        }
+
+
 
 
 
